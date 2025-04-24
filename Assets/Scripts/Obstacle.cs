3d3f6ead1590942a0b1c1f251b5ccb0ddf5378fe
Briant Anaya <briantanaya@yahoo.com>
2025-04-23 22:47:45 -0700

    Created base class
    I created the base class that includes a private backing field with public virtual properties that the children can access. I'm trying to force the children to set up their own unique prefabs, crash sounds, and speed. The spawn manager would then use that.

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public abstract class Obstacle : MonoBehaviour
+{
+    [SerializeField] private GameObject obstacle;
+    [SerializeField] private float obstacleSpeed;
+    [SerializeField] private AudioClip obstacleClip;
+
+    public virtual GameObject pObstacle { get => obstacle; protected set => obstacle = value; }
+    public virtual float pObstacleSpeed { get => obstacleSpeed; protected set => obstacleSpeed = value; }
+    public virtual AudioClip pObstacleClip { get => obstacleClip; protected set => obstacleClip = value; }
+
+
+
+    /*
+    public abstract GameObject obstacle { get; protected set; }
+    public abstract float obstacleSpeed { get; protected set; }
+    public abstract AudioClip obstacleClip { get; protected set; }
+    */
+
+}
---
+++ Assets/Scripts/RockObstacle.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class RockObstacle : Obstacle
+{
+    //public override GameObject pObstacle { get; protected set; }
+    public override float pObstacleSpeed { get; protected set; } = -3.5f;
+    //public override AudioClip pObstacleClip { get; protected set; }
+}
